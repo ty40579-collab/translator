99dd5a45d70f65d11135b4bb255ea7eee648f8e8
Nayana12345678! <nayana.833842@gmail.com>
2026-04-18 14:40:58 +0530

    added

---
+++ translator.py
@@ -0,0 +1,36 @@
+from deep_translator import GoogleTranslator
+from gtts import gTTS
+import os
+
+def translate_text(text, target_lang):
+    return GoogleTranslator(source='auto', target=target_lang).translate(text)
+
+def speak_text(text, lang, filename="speech.mp3"):
+    tts = gTTS(text=text, lang=lang)
+    tts.save(filename)
+
+    # This opens the file in default media player (Windows)
+    os.system(f"start {filename}")
+
+def main():
+    print("1: Kannada → English")
+    print("2: English → Kannada")
+
+    choice = input("Choose option: ")
+    text = input("Enter text: ")
+
+    if choice == "1":
+        translated = translate_text(text, "en")
+        print("Translated:", translated)
+        speak_text(translated, "en")
+
+    elif choice == "2":
+        translated = translate_text(text, "kn")
+        print("Translated:", translated)
+        speak_text(translated, "kn")
+
+    else:
+        print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
